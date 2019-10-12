1df2df0207929722d3064b456bda6c36bae1edbb
Andrei Airinei <airinei.andrei@gmail.com>
2019-10-12 22:07:37 +0100

    Added Top and bottom borders to header an footer, finnished footer

--- src/components/products-footer/products-footer.component.jsx
+++ src/components/products-footer/products-footer.component.jsx
@@ -7,37 +7,36 @@ import './products-footer.styles.scss';
 const ProductsFooter = () => {
   return (
     <div className='products-footer'>
-      <div className='footer-left'>
-        <div className='logo'>
-          <Link to='/'>
-            <img
-              src='//cdn.shopify.com/s/files/1/0037/8745/6612/files/monaki-logo_150x.png?v=1544692591'
-              alt='Monaki Home Logo'
-            />
-          </Link>
-        </div>
-        <div className='copyright'>
-          <p>Copyright 2019. All rights reserved.</p>
-        </div>
-      </div>
-      <div className='footer-right'>
-        <div className='commercials'>
-          <div className='comm1'>
-            <img
-              src='//cdn.shopify.com/s/files/1/0037/8745/6612/files/security.png?v=1559136431'
-              alt='Commercials'
-            />
-          </div>
-          <div className='comm2'>
-            <img
-              src='//cdn.shopify.com/s/files/1/0037/8745/6612/files/gr.png?v=1559136418'
-              alt='Commercials'
-            />
-          </div>
-          <div className='header-order-now'>
-            <Link to='/cart' className='header-btn button-fill-left'>
-              ORDER NOW
+      <div className='products-footer-wrapper'>
+        <div className='footer-left'>
+          <div className='logo'>
+            <Link to='/'>
+              <img
+                src='//cdn.shopify.com/s/files/1/0037/8745/6612/files/monaki-logo_150x.png?v=1544692591'
+                alt='Monaki Home Logo'
+              />
             </Link>
+          </div>
+          <div className='copyright'>
+            <p>Copyright 2019. All rights reserved.</p>
+          </div>
+        </div>
+        <div className='footer-right'>
+          <div className='commercials'>
+            <div className='comm1'>
+              <img
+                src='//cdn.shopify.com/s/files/1/0037/8745/6612/files/security.png?v=1559136431'
+                alt='Commercials'
+              />
+            </div>
+            <div className='comm2'>
+              <img
+                src='//cdn.shopify.com/s/files/1/0037/8745/6612/files/gr.png?v=1559136418'
+                alt='Commercials'
+              />
+            </div>
+            <Link to='/terms-conditions'>Terms</Link>
+            <Link to='/privacy-policy'>Privacy Policy</Link>
           </div>
         </div>
       </div>
--- src/components/products-header/products-header.component.jsx
+++ src/components/products-header/products-header.component.jsx
@@ -7,31 +7,33 @@ import './products-header.styles.scss';
 const ProductsHeader = () => {
   return (
     <div className='products-header'>
-      <div className='logo'>
-        <Link to='/'>
-          <img
-            src='//cdn.shopify.com/s/files/1/0037/8745/6612/files/monaki-logo_150x.png?v=1544692591'
-            alt='Monaki Home Logo'
-          />
-        </Link>
-      </div>
-      <div className='commercials'>
-        <div className='comm1'>
-          <img
-            src='//cdn.shopify.com/s/files/1/0037/8745/6612/files/security.png?v=1559136431'
-            alt='Commercials'
-          />
-        </div>
-        <div className='comm2'>
-          <img
-            src='//cdn.shopify.com/s/files/1/0037/8745/6612/files/gr.png?v=1559136418'
-            alt='Commercials'
-          />
-        </div>
-        <div className='header-order-now'>
-          <Link to='/cart' className='header-btn button-fill-left'>
-            ORDER NOW
+      <div className='products-header-wrapper'>
+        <div className='logo'>
+          <Link to='/'>
+            <img
+              src='//cdn.shopify.com/s/files/1/0037/8745/6612/files/monaki-logo_150x.png?v=1544692591'
+              alt='Monaki Home Logo'
+            />
           </Link>
+        </div>
+        <div className='commercials'>
+          <div className='comm1'>
+            <img
+              src='//cdn.shopify.com/s/files/1/0037/8745/6612/files/security.png?v=1559136431'
+              alt='Commercials'
+            />
+          </div>
+          <div className='comm2'>
+            <img
+              src='//cdn.shopify.com/s/files/1/0037/8745/6612/files/gr.png?v=1559136418'
+              alt='Commercials'
+            />
+          </div>
+          <div className='header-order-now'>
+            <Link to='/cart' className='header-btn button-fill-left'>
+              ORDER NOW
+            </Link>
+          </div>
         </div>
       </div>
     </div>
