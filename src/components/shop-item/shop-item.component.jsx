7ac0a454f71b57140af4ab0f857953ca5ab4ea8f
Andrei Airinei <airinei.andrei@gmail.com>
2019-10-15 13:06:07 +0100

    Added shop-item component without animations

---
+++ src/components/shop-item/shop-item.component.jsx
@@ -0,0 +1,71 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import './shop-item.styles.scss';
+
+const ShopItem = () => {
+  return (
+    <div className='shop-item'>
+      <div className='shop-item-container'>
+        <div className='product-image'>
+          <div className='product-image-bubble'>
+            <img
+              src='//cdn.shopify.com/s/files/1/0037/8745/6612/products/v-3178592__-1005623596.jpg?v=1559136177'
+              alt='Showerhead'
+            />
+          </div>
+        </div>
+        <div className='product-details'>
+          <span className='product-title'>Monaki® Home Original</span>
+          <span className='product-title2'>
+            Gold Ionic High-Pressure Showerhead
+          </span>
+          <span className='product-label'>#1 Best Seller</span>
+          <ul className='product-checks'>
+            <li>
+              <img
+                src={require('../../assets/icon-checked.png')}
+                alt='icon-check'
+              />
+              Gorgeous Gold or Silver Finish
+            </li>
+            <li>
+              <img
+                src={require('../../assets/icon-checked.png')}
+                alt='icon-check'
+              />
+              35% Less Water Waste
+            </li>
+            <li>
+              <img
+                src={require('../../assets/icon-checked.png')}
+                alt='icon-check'
+              />
+              Increases Water Pressure Drastically
+            </li>
+            <li>
+              <img
+                src={require('../../assets/icon-checked.png')}
+                alt='icon-check'
+              />
+              Supreme Quality – Will Last Forever
+            </li>
+          </ul>
+          <div className='testimonial'>
+            <div className='testimonial-image'></div>
+            <div className='testimonial-message'>
+              "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Itaque
+              totam aperiam ea fugiat sapiente, nesciunt ullam minima, obcaecati
+              reiciendis vero quidem."
+            </div>
+          </div>
+          <Link to='/cart' className='buy-now-btn button-fill-bottom flipInX'>
+            BUY NOW
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ShopItem;
--- src/pages/products/products.component.jsx
+++ src/pages/products/products.component.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import ProductsHeader from '../../components/products-header/products-header.component';
+import ShopItem from '../../components/shop-item/shop-item.component';
 import ProductsFooter from '../../components/products-footer/products-footer.component';
 
 import './products.styles.scss';
@@ -9,7 +10,8 @@ const ProductsPage = () => {
   return (
     <div className='products'>
       <ProductsHeader></ProductsHeader>
-      <h1>ProductsPage</h1>
+      <ShopItem></ShopItem>
+      <ShopItem></ShopItem>
       <ProductsFooter></ProductsFooter>
     </div>
   );
